176ac2893f64168f6bdd89ec67be0c64bf0aec89
David Fischer <david@fisch3r.net>
2023-06-12 19:35:37 +0200

    Fix paths

--- 2_container_beat.tf
+++ 2_container_beat.tf
@@ -30,13 +30,13 @@ resource "docker_container" "celery_beat" {
 
   volumes {
     container_path = local.container_media_directory
-    host_path      = local.container_media_directory
+    host_path      = local.media_directory
     read_only      = true
   }
 
   volumes {
     container_path = local.container_static_directory
-    host_path      = local.container_static_directory
+    host_path      = local.static_directory
     read_only      = true
   }
 }
